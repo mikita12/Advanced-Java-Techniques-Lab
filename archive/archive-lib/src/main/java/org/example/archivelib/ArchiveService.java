a18c8e97ae066303308d81efc850b33b38ee86d8
mikita <patronikm@gmail.com>
2026-03-03 21:35:33 +0100

    feat: implemented method calculateSha256

--- archive/archive-lib/src/main/java/org/example/archivelib/ArchiveService.java
+++ archive/archive-lib/src/main/java/org/example/archivelib/ArchiveService.java
@@ -1,8 +1,40 @@
 package org.example.archivelib;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
 public class ArchiveService {
 
-    public void test() {
-        System.out.println("Library działa");
+
+    public String calculateSha256(Path path) throws IOException {
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+
+            try (InputStream is = Files.newInputStream(path)) {
+
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    digest.update(buffer, 0, bytesRead);
+                }
+
+                byte[] hash = digest.digest();
+                StringBuilder hashHex = new StringBuilder();
+                for(byte b : hash){
+                    hashHex.append(String.format("%02x", b));
+                }
+                return hashHex.toString();
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
